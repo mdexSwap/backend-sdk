0d4da15d468a2312d7fade2fa7a5039474c9d5b0
zizhenli <snile657@outlook.com>
2022-03-24 13:15:16 +0800

    feat: 添加generateUuid方法

--- src/utils/common.util.ts
+++ src/utils/common.util.ts
@@ -1,4 +1,5 @@
 import * as _ from 'lodash';
+import Snowflake from '@axihe/snowflake';
 
 export const IsNumberString = (value): boolean => {
   if (_.isString(value) && value !== '') {
@@ -11,3 +12,15 @@ export const IsNumberString = (value): boolean => {
 export function hasDuplicates(a) {
   return _.uniq(a).length !== a.length;
 }
+
+const generateUuid = () => {
+  const config = {
+    worker_id: 16, // 0-31
+    datacenter_id: 18, // 0-31
+  };
+  const idWorker = new Snowflake(config.worker_id, config.datacenter_id);
+  const id = idWorker.nextId();
+  return id.toString();
+};
+
+console.log(generateUuid());
